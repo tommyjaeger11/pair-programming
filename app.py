927caa7f522e4eab39e3830f4f32c0a4008e49e7
Tommy Jaeger <tjjaeger@uiowa.edu>
2025-04-28 14:42:03 -0500

    update api_url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = "seasdad(*2sffcra01^23sdet"
 CORS(app)
 
 # Get this URL from the Azure Overview page of your API web app
-api_url = "https://colbert-api-fpd0evcqfneja4cj.eastus-01.azurewebsites.net"  # base url for API endpoints
+api_url = "https://tjjaeger-api-c2fbaab0g0hyekd3.westus-01.azurewebsites.net"  # base url for API endpoints
 
 
 # main index page route
